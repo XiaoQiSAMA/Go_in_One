a728ad87b96bf7e7582367e2047d8045b18f5dc2
XiaoQiSAMA <327457663@qq.com>
2023-12-07 15:04:21 +0800

    组合与内嵌扩展

--- practice/src/tree/entry/entry.go
+++ practice/src/tree/entry/entry.go
@@ -1,9 +1,55 @@
 package main
 
-import "learngo/src/tree"
+import (
+	"fmt"
+	"learngo/src/tree"
+)
+
+// 扩展tree包中的node
+// // 1. 组合
+// type MyTreeNode struct {
+// 	node *tree.Node
+// }
+
+// func (myNode *MyTreeNode) preOrder() {
+// 	if myNode == nil || myNode.node == nil {
+// 		return
+// 	}
+
+// 	left := MyTreeNode{myNode.node.Left}
+// 	right := MyTreeNode{myNode.node.Right}
+
+// 	left.preOrder()
+// 	right.preOrder()
+// 	myNode.node.Print()
+// }
+
+// 3. 内嵌Embedding(类似继承)
+type MyTreeNode struct {
+	*tree.Node
+}
+
+func (myNode *MyTreeNode) preOrder() {
+	if myNode == nil || myNode.Node == nil {
+		return
+	}
+
+	left := MyTreeNode{myNode.Left}
+	right := MyTreeNode{myNode.Right}
+
+	left.preOrder()
+	right.preOrder()
+	myNode.Print()
+}
+
+func (myNode *MyTreeNode) Traverse() {
+	fmt.Println("This is a shadowed method")
+}
 
 func main() {
-	root := tree.Node{Val: 3}
+	// root := tree.Node{Val: 3}
+	root := MyTreeNode{&tree.Node{Val: 3}} // Embedding
+
 	root.Left = &tree.Node{}
 	root.Right = &tree.Node{5, nil, nil}
 	root.Right.Left = new(tree.Node)
@@ -24,12 +70,24 @@ func main() {
 	// pRoot.setVal(300)
 	// pRoot.print()
 
-	root.Traverse()
+	// root.Traverse()	// 重载函数
+	root.Node.Traverse()
+	fmt.Println()
+
+	// myRoot := MyTreeNode{&root}
+	// myRoot.preOrder()
+	root.preOrder() // Embedding
+	fmt.Println()
 	// nodes := []treeNode{
 	// 	{val: 3},
 	// 	{},
 	// 	{6, nil, nil},
 	// }
 	// fmt.Println(nodes)
+
+	// // Embedding与继承的区别:无法用父类指针指向子类
+	// // cannot use &root (value of type *MyTreeNode) as *tree.Node value in
+	// var baseRoot *tree.Node
+	// baseRoot = &root
 
 }
